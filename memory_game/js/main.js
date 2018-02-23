e4f6795438b5d86cc016d99877b97bc553a6f93c
Meena Jim George <meena@Meenas-MacBook-Pro.local>
2018-02-23 12:43:40 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,9 +7,37 @@ console.log("User flipped" + cardOne);
 console.log("User flipped" + cardThree);*/
 var cards=["queen","queen","king","king"];
 var cardsInPlay=[];
-var cardOne=cards[0];
+var checkForMatch=function()
+{
+if(cardsInPlay.length===2)
+ {
+ 	 if(cardsInPlay[0]===cardsInPlay[1])
+ 	 {
+ 	 	alert("You found a match!");
+ 	 }
+ 	 else
+ 	 {
+ 	 	alert("Sorry, try again");
+ 	 }
+ }
+}
 
+var flipCard=function(cardId){
+console.log("User Flipped"+cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+/*var cardOne=cards[0];
 cardsInPlay.push(cardOne);
+console.log("User flipped"  +  " " + cardOne);
+ var cardTwo=cards[2];
+ cardsInPlay.push(cardTwo);
+ console.log("User flipped" + " " + cardTwo);*/
+checkForMatch();
+ 
+}
+flipCard(0);
+flipCard(2);
+
+/*cardsInPlay.push(cardOne);
 console.log("User flipped"  +  " " + cardOne);
  var cardTwo=cards[2];
  cardsInPlay.push(cardTwo);
@@ -24,4 +52,4 @@ console.log("User flipped"  +  " " + cardOne);
  	 {
  	 	alert("Sorry, try again");
  	 }
- }
+ }*/
